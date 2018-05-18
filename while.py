25b0af2fd7906e842d94a284be0e2ae18b8d287b
himanitilayacha <39405879+himanitilayacha@users.noreply.github.com>
2018-05-18 17:37:38 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,27 @@
+"""
+i = 1
+while(i < 5):
+    i=i+1
+    pri
+"""    
+string= " himani , princy , kavi"
+i=-1
+string[i]
+while i < (len(string) - 1):
+    i=i+1
+    if( string[i] != " ") and (string[i] != ","):
+        print string[i]
+print "......................................"
+
+
+string= " himani , princy , kavi"
+i=-1
+string[i]
+while i < (len(string) - 1):
+    i=i+1
+    if( string[i] == " ") or (string[i] == ","):
+        continue
+    print string[i]
+        
+    
+
