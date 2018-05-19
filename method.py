d301a0b1553d38c98355bb9119688ed9d00852af
himanitilayacha <39405879+himanitilayacha@users.noreply.github.com>
2018-05-19 10:18:03 +0530

    Add files via upload

---
+++ method.py
@@ -0,0 +1,29 @@
+lip="himani"
+li = lip.capitalize()
+
+
+
+def cap1(name, surname):
+	print name + " " + surname
+
+cap1("prashant", "tiwari")
+s = "prashant tiwari"
+t = "divyam bohra"
+cap1(s,t)
+
+def cap2(name, surname):
+
+	print name + " " + surname
+
+def cap3(name, surname):
+
+	return name + " " + surname
+
+surname = "prashant"
+name = "tiwari"
+cap2(surname,name)
+
+c = cap3("hjgj","hgdv ga")
+
+print c
+
