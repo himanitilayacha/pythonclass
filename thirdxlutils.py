b87eaffc981f5c37204e8fd82e8f1a7c5e9c1664
himanitilayacha <39405879+himanitilayacha@users.noreply.github.com>
2018-05-20 11:40:59 +0530

    Add files via upload

--- thirdxlutils.py
+++ thirdxlutils.py
@@ -1,13 +1,16 @@
 import xlrd,xlwt
 book=xlwt.Workbook()
 sheet=book.add_sheet("sheet1")
-for row in range(2):
-    for col in range(4):
+for row in range(3):
+    for col in range(3):
        sheet.write(row,col,raw_input("Entrer data : "))
 book.save("second.xls")
 
 from xlutils.copy import copy
 read_book=xlrd.open_workbook("second.xls")
 update_book=copy(read_book)
-sheet=update_book.get_sheet(0)
+sheet=update_book.add_sheet("Himani 2")
+
+update_book.save("second.xls")
+
 
