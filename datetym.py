9c0c178bcaeb293d12237f602d2a78d439d0d173
himanitilayacha <39405879+himanitilayacha@users.noreply.github.com>
2018-05-20 11:05:13 +0530

    Add files via upload

---
+++ datetym.py
@@ -0,0 +1,10 @@
+import datetime
+datetime.datetime.now()
+"""
+                     from datetime import datetime
+                      datetime.now()
+                      ""
+import math
+math.sqrt(s)
+from math import sqrt
+sqrt(16)
