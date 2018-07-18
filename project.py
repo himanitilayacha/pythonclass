f6e74cc122c0d73921a1bffcbf4a9466522dccb9
himanitilayacha <39405879+himanitilayacha@users.noreply.github.com>
2018-07-19 00:40:14 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 from Tkinter import *
 import random as r
-import RPi.GPIO as G
+
 from time import sleep
 
 color=['red','blue','green','brown','black','purple']
@@ -10,21 +10,16 @@ w.geometry("360x360")
 w.configure(background ='maroon')
 w.title('project')
 
-G.setmode(G.BOARD)
-G.setup(12,G.OUT)
 
 def glow():
 
-        G.output(12,True)
-	sleep(0.01)
+
         w.configure(bg=color[r.randint(0,8)])
 
 button1=Button(w,width=15 , text="ON",command=glow)
 button1.grid(row=1,column=1)
 
 def dim():
-
-        G.output(12,False)
         w.configure(bg=color[r.randint(0,8)])
 
 button2=Button(w,width=15 ,text="OFF", command=dim)
